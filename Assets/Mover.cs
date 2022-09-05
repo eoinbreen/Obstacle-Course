5065ff1cd9c9e118cabee8bafba9489c6a1e946d
eoinbreen <e.breen185@gmail.com>
2022-09-05 21:51:49 +0100

    Added Collisions with walls

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -11,17 +11,30 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MoveBlockie();
+    }
+
+    //Printing the instructions for the Game
+    void PrintInstructions() 
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Avoid all the obstacles and make it to the goal");
+        Debug.Log("Good Luck!");
+    }
+
+    //Moving Player
+    void MoveBlockie()
     {
         float xValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;//change x value with up and down keys, frame independent
         float zValue = -(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);//change z value with left and right keys, frame independent
 
         transform.Translate(xValue, 0, zValue);
     }
-
     
 }
